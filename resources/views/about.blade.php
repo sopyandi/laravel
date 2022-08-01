23a5bdfd8146305faaf592864787b1a968468374
sopyandi <sopyandirian12@gmail.com>
2022-08-01 14:27:23 +0700

    laravel 3

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,6 +4,6 @@
 <h1>Halaman About</h1>
 <h3>{{$name}}</h3>
  <p>{{$gmail}}</p>
- <img src="img/{{$img}}" alt="{{$name}}" style="width:200px;">
+ <img src="img/{{$img}}" alt="{{$name}}" style="width:200px;" class="img-thubnail rounded-circle">
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,76 +33,19 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', function () {
-    $blog_post = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-slug-pertama",
-            "author" => "Rian Sopyandi",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam asperiores soluta
-            voluptas eum commodi inventore reiciendis illum expedita, mollitia
-            laboriosam fuga vel nesciunt corrupti! Aliquid tenetur debitis quidem ut delectus
-            eos eius fuga fugiat architecto, amet corrupti ipsa minima saepe? Possimus corrupti aspernatur
-             nihil sit tenetur debitis sint harum beatae asperiores qui dolor totam iure aut molestias 
-             excepturi, repellendus quasi earum, accusamus in dolorum non. Odio, sint rem. Quas blanditiis
-              consequuntur mollitia sed expedita itaque quam maiores quaerat quisquam delectus!"
-        ],
-        [
-            "title" => "Judul Post Selanjutnya",
-            "slug" => "judul-slug-kedua",
-            "author" => "Anisa Fitria",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam asperiores soluta
-            voluptas eum commodi inventore reiciendis illum expedita, mollitia
-            laboriosam fuga vel nesciunt corrupti! Aliquid tenetur debitis quidem ut delectus
-            eos eius fuga fugiat architecto, amet corrupti ipsa minima saepe? Possimus corrupti aspernatur
-             nihil sit tenetur debitis sint harum beatae asperiores qui dolor totam iure aut molestias 
-             excepturi, repellendus quasi earum, accusamus in dolorum non. Odio, sint rem. Quas blanditiis
-              consequuntur mollitia sed expedita itaque quam maiores quaerat quisquam delectus!"
-        ]
-        ];
-        
+    
     return view('blog', [
         "title"=>"Blog",
-        "blog" => $blog_post
+        "blog" => Post::all()
+        
     ]);
 });
 
 // routs untuk single blog di halaman blog
 Route::get('blog/{slug}', function($slug){
-    $blog_post = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-slug-pertama",
-            "author" => "Rian Sopyandi",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam asperiores soluta
-            voluptas eum commodi inventore reiciendis illum expedita, mollitia
-            laboriosam fuga vel nesciunt corrupti! Aliquid tenetur debitis quidem ut delectus
-            eos eius fuga fugiat architecto, amet corrupti ipsa minima saepe? Possimus corrupti aspernatur
-             nihil sit tenetur debitis sint harum beatae asperiores qui dolor totam iure aut molestias 
-             excepturi, repellendus quasi earum, accusamus in dolorum non. Odio, sint rem. Quas blanditiis
-              consequuntur mollitia sed expedita itaque quam maiores quaerat quisquam delectus!"
-        ],
-        [
-            "title" => "Judul Post Selanjutnya",
-            "slug" => "judul-slug-kedua",
-            "author" => "Anisa Fitria",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam asperiores soluta
-            voluptas eum commodi inventore reiciendis illum expedita, mollitia
-            laboriosam fuga vel nesciunt corrupti! Aliquid tenetur debitis quidem ut delectus
-            eos eius fuga fugiat architecto, amet corrupti ipsa minima saepe? Possimus corrupti aspernatur
-             nihil sit tenetur debitis sint harum beatae asperiores qui dolor totam iure aut molestias 
-             excepturi, repellendus quasi earum, accusamus in dolorum non. Odio, sint rem. Quas blanditiis
-              consequuntur mollitia sed expedita itaque quam maiores quaerat quisquam delectus!"
-        ]
-        ];
-        $wadah = [];
-        foreach($blog_post as $data ){
-        if ($data['slug'] === $slug) {
-            $wadah = $data;
-        }
-      }
-      
+
      return view('single',[
         "title" => "Single Blog",
-        "single_blog" => $wadah
+        "single_blog" => Post::find($slug)
      ]);
 });
